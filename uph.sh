dc7edb42717979aa0039bce1fada5da0678c4a21
miroljub1995 <46632388+miroljub1995@users.noreply.github.com>
2020-04-29 21:57:22 +0200

    Update uph.sh

--- uph.sh
+++ uph.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.profile
 nvm install 12.16.3
