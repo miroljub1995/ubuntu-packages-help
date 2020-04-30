e46f37f7fde4bf04edff1f861b0ad48b581c768b
Miki <miroljub1995@gmail.com>
2020-04-30 19:53:29 +0200

    added sumo to apps

---
+++ apps/sumo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+sudo add-apt-repository ppa:sumo/stable
+sudo apt-get update
+sudo apt-get install sumo sumo-tools sumo-doc
+echo -e '\n# add SUMO_HOME var\nexport SUMO_HOME="/usr/share/sumo/"\n# ####\n' >> ~/.bashrc
